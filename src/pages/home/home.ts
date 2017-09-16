a3fe19695a619cea62686b7411e289135e280b60
Cristhian Arostegui <cr.arostegui@gmail.com>
2017-09-16 19:12:13 -0400

    - New place component added. - Navigation from "Home" page to "new place page" added.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { NewPlacePage } from '../new-place/new-place';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,7 +10,10 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+  }
 
+  onLoadNewPlace(){
+    this.navCtrl.push(NewPlacePage);
   }
 
 }
